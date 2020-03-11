5dbe59b189c39a7d95c797ee07c359fc5b89e9fa
HissZxy <956527824@qq.com>
2020-03-11 21:02:03 +0800

    java 牛客网练习 删除公共字符

---
+++ DeleteGongGongZiFu.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+            String pattern = "[" + s2 + "]";
+            String result = s1.replaceAll(pattern,"");
+            System.out.println(result);
+        }
+    }
+}
