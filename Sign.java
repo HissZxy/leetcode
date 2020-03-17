972c4b471b3f5b2dec91c025d3c69bf6e7cb484e
HissZxy <956527824@qq.com>
2020-03-17 22:34:13 +0800

    java 牛客网练习 sign函数

---
+++ Sign.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class signº¯Êý {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double x=sc.nextDouble();
+		if(x<0) {
+			System.out.println("-1");
+		}
+		if(x==0) {
+			System.out.println("0");
+		}
+		if(x>0) {
+			System.out.println("1");
+		}
+	}
+
+
+}
