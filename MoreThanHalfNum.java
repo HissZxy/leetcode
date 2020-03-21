3d4e6f1aad231900ff0affd2dfa6eb193db4df49
HissZxy <956527824@qq.com>
2020-03-21 11:42:11 +0800

    java 牛客网练习 出现超过一半的数字

---
+++ MoreThanHalfNum.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+ public class Solution {
+    public int MoreThanHalfNum_Solution(int [] array) {
+        Arrays.sort(array);
+        int count=0;
+         
+        for(int i=0;i<array.length;i++){
+            if(array[i]==array[array.length/2]){
+                count++;
+            }
+        }
+        if(count>array.length/2){
+            return array[array.length/2];
+        }else{
+            return 0;
+        }
+         
+    }
+}
