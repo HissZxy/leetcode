cc687a1e6a27d50b0f174d248fe08fe107cf1246
HissZxy <956527824@qq.com>
2020-03-27 20:24:42 +0800

    Java leetcode练习 反转数字

---
+++ FanZhuanShuZi.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+String xString = Integer.toString(x);
+        String string = xString;
+        int flag = 1;
+        if (x < 0) {
+            flag = -1;
+            string = xString.substring(1);
+        }
+        try {
+            return Integer.valueOf((new StringBuilder(string)).reverse().toString()) * flag;
+        }catch (Exception e){
+            return 0;
+        }
+    }
