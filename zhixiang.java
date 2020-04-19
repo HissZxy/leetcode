ec16ef79877db5b11dbb61f883e8c57faf42cbb2
HissZxy <956527824@qq.com>
2020-04-19 20:43:18 +0800

    Java 题目练习 Zuozhixiang

---
+++ zhixiang.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+
+
+    /*请完成下面这个函数，实现题目要求的功能
+    当然，你也可以不按照下面这个模板来作答，完全按照自己的想法来 ^-^
+    ******************************开始写代码******************************/
+
+    /******************************结束写代码******************************/
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int l;
+        l = in.nextInt();
+        int arr[] = new int[l];
+        int arr1[] = new int[l];
+        for(int i = 0;i<arr.length ;i++){
+            arr[i]=in.nextInt();
+            }
+        for(int j = 0; j<arr1.length;j++){
+            arr1[j]=in.nextInt();
+        }
+        }
+    
+
+    }
