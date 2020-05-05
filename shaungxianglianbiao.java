74bad1797cd118758f1ed4f8aa68feb58670e124
HissZxy <956527824@qq.com>
2020-05-05 17:30:57 +0800

    Java 牛客网剑指offer 双线链表二叉搜索树

---
+++ shaungxianglianbiao.java
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public TreeNode Convert(TreeNode root)
+    {
+         
+        if(root==null)return null;
+        if(root.left==null&&root.right==null)return root;
+        TreeNode left=Convert(root.left);
+        TreeNode p=left;
+        while(p!=null&&p.right!=null)
+        {
+            p=p.right;
+        }
+        if(left!=null)
+        {
+         p.right=root;
+          root.left=p;
+        }
+       TreeNode right=Convert(root.right);
+        if(right!=null)
+         {
+            root.right=right;
+            right.left=root;
+        }
+         
+      return  left!=null?left:root; 
+    }
+}
