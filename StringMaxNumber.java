e10a5b1a7a11309b0728ce4ac139fcd01430e5dc
HissZxy <956527824@qq.com>
2020-06-02 23:37:19 +0800

    Java 牛客网练习 找出字符串中最长的数字串

---
+++ StringMaxNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      while(scanner.hasNext()){
+          String str = scanner.nextLine();
+          int max = 0;
+          int count=0;
+          int end=0;
+          for(int i=0;i<str.length();i++){
+              if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+                  count++;
+                  if(max<count){
+                      max= count;
+                      end = i;
+                  }
+              }else{
+                  count = 0;
+              }
+          }
+          System.out.println(str.substring(end-max+1,end+1));
+      }
+  }
+}
