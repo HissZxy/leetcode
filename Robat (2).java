9fc716d8bb9b0618467f1bb59fa51190ac974d73
HissZxy <956527824@qq.com>
2020-06-10 22:36:45 +0800

    Java 牛客网练习 机器人走方格I

---
+++ Robat (2).java
@@ -0,0 +1,16 @@
+
+public int countWays(int x, int y) {
+        // write code here
+        int[][] dp = new int[13][13];
+        for (int i = 1; i < y; i++)
+            dp[0][i] = 1;
+         
+        for (int i = 1; i < x; i++)
+            dp[i][0] = 1;
+         
+        for (int i = 1; i < x; i++)
+            for (int j = 1; j < y; j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+         
+        return dp[x-1][y-1];
+    }
