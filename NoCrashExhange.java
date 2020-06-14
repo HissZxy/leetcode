f2be019e0d156401caa7dce3fca057bb4ba48f79
HissZxy <956527824@qq.com>
2020-06-14 21:27:41 +0800

    Java 牛客网练习 无缓存交换

---
+++ NoCrashExhange.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+ 
+public class Exchange {
+    public int[] exchangeAB(int[] AB) {
+        // write code here
+         
+        AB[0]=AB[0]+AB[1];
+        AB[1]=AB[0]-2*AB[1];
+        AB[0]=(AB[0]-AB[1])/2;
+        AB[1]=AB[0]+AB[1];
+        return AB;
+         
+    }
+}
