e423d561719fd5cc9b17c66930b52ffcbc9283d1
HissZxy <48994709+HissZxy@users.noreply.github.com>
2020-07-09 21:20:20 +0800

    JAVA 牛客网练习 股市

--- GuShi.java
+++ GuShi.java
@@ -1,22 +1,22 @@
 public class Solution {
-
-
-public int calculateMax(int[] prices) {
-       int firstBuy = Integer.MAX_VALUE;//第一次买入的价格
-       // 接下来都是买入卖出之后的收益
-       int afterFirstSell = 0;
-       int afterSecondBuy = Integer.MIN_VALUE;
-       int afterSecondSell = 0;
-        
-       for (int curPrice: prices){
-           //第一次买入的价格应该是越小越好，最好是负数，倒贴钱
-           firstBuy = Math.min(firstBuy, curPrice);
-           //第一次卖出后的收益，等于当前价格减去第一次买入价格，越高越好
-           afterFirstSell = Math.max(afterFirstSell, curPrice - firstBuy);
-           //第二次买入后的收益，等于第一次卖出后的收益减去当前价格，越高越好
-           afterSecondBuy = Math.max(afterSecondBuy, afterFirstSell - curPrice);
-           //第二次卖出后的收益，等于第二次买入后的收益加上当前价格，越高越好
-           afterSecondSell = Math.max(afterSecondSell, afterSecondBuy + curPrice);
-       }
-       return afterSecondSell;
-   }
+     
+    public int calculateMax(int[] prices) {
+        if(prices==null || prices.length<2)return 0;
+        int sum=0;
+        for(int i=1;i<prices.length;i++){
+            int temp=getMax(prices,0,i)+getMax(prices,i+1,prices.length-1);
+            if(temp>sum)sum=temp;
+        }
+        return sum;
+    }
+    public static int getMax(int[] prices,int left,int right){
+        if(left>=prices.length)return 0;
+        int Min=prices[left];
+        int ret=0;
+        for(int i=left+1;i<=right;i++){
+            Min=Math.min(prices[i],Min);
+            ret=Math.max(ret,prices[i]-Min);
+        }
+        return ret;
+    }
+}
