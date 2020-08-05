78ccf0647a8727f79e4a672f3144467de8a174a3
HissZxy <48994709+HissZxy@users.noreply.github.com>
2020-08-05 20:30:55 +0800

    java leetcode练习 EXCel对应

---
+++ Excel.java
@@ -0,0 +1,12 @@
+public String convertToTitle(int n) {
+        if (n <= 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            n--;
+            sb.append((char) (n % 26 + 'A'));
+            n =n / 26;
+        }
+        return sb.reverse().toString();
+    }
